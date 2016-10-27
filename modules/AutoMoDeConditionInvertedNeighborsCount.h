9c037a90b9cd3e7c6cf6742d3b149b254b42b741
Antoine <aligot@ulb.ac.be>
2016-10-27 09:07:47 +0200

    Implementation of NeighborsCount and InvertedNeighborsCount

--- modules/AutoMoDeConditionInvertedNeighborsCount.h
+++ modules/AutoMoDeConditionInvertedNeighborsCount.h
@@ -20,6 +20,10 @@ namespace argos {
 			virtual bool Verify();
 			virtual void Reset();
 			virtual void Init();
+			
+		private:
+			Real m_fParameterEta;
+			UInt8 m_unParameterXi;
 	};
 }
 
--- modules/AutoMoDeConditionNeighborsCount.cpp
+++ modules/AutoMoDeConditionNeighborsCount.cpp
@@ -7,11 +7,20 @@
 
  namespace argos {
 
+	/****************************************/
+	/****************************************/
+
 	AutoMoDeConditionNeighborsCount::AutoMoDeConditionNeighborsCount() {
 		m_strLabel = "NeighborsCount";
 	}
 
+	/****************************************/
+	/****************************************/
+
 	AutoMoDeConditionNeighborsCount::~AutoMoDeConditionNeighborsCount() {}
+
+	/****************************************/
+	/****************************************/
 
 	AutoMoDeConditionNeighborsCount::AutoMoDeConditionNeighborsCount(AutoMoDeConditionNeighborsCount* pc_condition) {
 		m_strLabel = pc_condition->GetLabel();
@@ -21,21 +30,44 @@
 		m_unToBehaviourIndex = pc_condition->GetExtremity();
 		m_mapParameters = pc_condition->GetParameters();
 	}
+	
+	/****************************************/
+	/****************************************/
 
 	AutoMoDeConditionNeighborsCount* AutoMoDeConditionNeighborsCount::Clone() {
 		return new AutoMoDeConditionNeighborsCount(*this);
 	}
+	
+	/****************************************/
+	/****************************************/
 
 	bool AutoMoDeConditionNeighborsCount::Verify() {
-		return true;
+		UInt32 unNumberNeighbors = m_pcRobotDAO->GetNumberNeighbors();
+		Real fProbability = (1/(1 + exp(m_fParameterEta * (m_unParameterXi - unNumberNeighbors))));
+		LOG << fProbability << std::endl;
+		return EvaluateBernoulliProbability(fProbability);
 	}
+
+	/****************************************/
+	/****************************************/
 
 	void AutoMoDeConditionNeighborsCount::Reset() {
 
 	}
 	
+	/****************************************/
+	/****************************************/
+	
 	void AutoMoDeConditionNeighborsCount::Init() {
-
+		std::map<std::string, Real>::iterator itEta = m_mapParameters.find("w");
+		std::map<std::string, Real>::iterator itXi = m_mapParameters.find("p");
+		if ((itEta != m_mapParameters.end()) && (itXi != m_mapParameters.end())) {
+			m_fParameterEta = itEta->second;
+			m_unParameterXi = itXi->second;
+		} else {
+			LOGERR << "[FATAL] Missing parameter for the following condition:" << m_strLabel << std::endl;
+			THROW_ARGOSEXCEPTION("Missing Parameter");
+		}
 	}
 
  }
