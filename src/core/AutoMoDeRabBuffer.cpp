9c20899067b378fc51f28f042fcb6905d2dc2023
Antoine Ligot <aligot@ulb.ac.be>
2018-03-09 09:13:49 +0100

    Fernando's fix to range and bearing buffer

--- src/core/AutoMoDeRabBuffer.cpp
+++ src/core/AutoMoDeRabBuffer.cpp
@@ -36,12 +36,17 @@ namespace argos {
   /****************************************/
 
   void AutoMoDeRabBuffer::Update() {
-    m_unCurrentTime += 1;
-    for (UInt32 i = 0; i < m_vecBufferElements.size(); i++) {
-      if (m_vecBufferElements.at(i).second < (m_unCurrentTime - m_unMaxTimeToLive)) {
-        m_vecBufferElements.erase(m_vecBufferElements.begin() + i);
+    if (m_unCurrentTime >= m_unMaxTimeToLive) {
+      UInt32 i = 0;
+      while (i < m_vecBufferElements.size()) {
+        if (m_vecBufferElements.at(i).second < (m_unCurrentTime - m_unMaxTimeToLive)) {
+          m_vecBufferElements.erase(m_vecBufferElements.begin() + i);
+        } else {
+          i += 1;
+        }
       }
     }
+    m_unCurrentTime += 1;
   }
 
   /****************************************/
