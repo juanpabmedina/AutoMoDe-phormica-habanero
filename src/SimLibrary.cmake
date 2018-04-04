e33386d8f2625a11694a5d26996ef0126c5f9315
Antonio Ligota <aligot@ulb.ac.be>
2018-04-04 08:10:43 +0200

    New executable that only parse the fsm description and opens it into firefox

--- src/SimLibrary.cmake
+++ src/SimLibrary.cmake
@@ -60,5 +60,7 @@ target_link_libraries(automode argos3plugin_${ARGOS_BUILD_FOR}_epuck)
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
 add_executable(automode_main AutoMoDeMain.cpp)
-target_link_libraries(automode_main automode argos3core_${ARGOS_BUILD_FOR} argos3plugin_${ARGOS_BUILD_FOR}_epuck)
+target_link_libraries(automode_main automode argos3core_${ARGOS_BUILD_FOR} argos3plugin_${ARGOS_BUILD_FOR}_epuck argos3_demiurge_loop_functions)
 
+add_executable(visualize_fsm AutoMoDeVisualizeFSM.cpp)
+target_link_libraries(visualize_fsm automode argos3core_${ARGOS_BUILD_FOR} argos3plugin_${ARGOS_BUILD_FOR}_epuck)
